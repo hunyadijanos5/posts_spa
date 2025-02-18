9e72408c0c6093f42a1c6076c76aeeec32312f53
hunyiOfficial <hunyadi.janos5@gmail.com>
2025-02-18 21:43:01 +0100

    add comments, posts, and tags data files & refactor data retrieval

--- app/store/index.js
+++ app/store/index.js
@@ -2,8 +2,11 @@ const fs = require('fs');
 const path = require('path');
 
 const getData = () => {
-    const data = fs.readFileSync(path.join(__dirname, './storage.json'));
-    return JSON.parse(data);
-};
+    const posts = JSON.parse(fs.readFileSync(path.join(__dirname, 'posts.json'), 'utf8')).posts;
+    const comments = JSON.parse(fs.readFileSync(path.join(__dirname, 'comments.json'), 'utf8')).comments;
+    const tags = JSON.parse(fs.readFileSync(path.join(__dirname, 'tags.json'), 'utf8')).tags;
+
+    return { posts, comments, tags };
+}
 
 module.exports = getData;
